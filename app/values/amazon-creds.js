c116057f8945a74293cfb878e9e93656149a140d
Chris Smalley <chrism.smalley@gmail.com>
2016-09-19 22:59:35 -0500

    fixed gitignore issue

--- app/values/amazon-creds.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-app.constant("AmazonCreds", {
-  publicKey: "AKIAJW2E7ZDX35WBOGQA",
-  privateKey: "gzhwKr4xwSMukcJJZ1BYFRt0Cdmrk8imWV2a/jra",
-  associateId: "csmalley89-20"
-});
-
-
