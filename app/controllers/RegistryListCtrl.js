22d39ccbafcbedfb15e1efe6685ed52d8c03e511
Chris Smalley <chrism.smalley@gmail.com>
2016-09-26 15:38:41 -0500

    havig issues

--- app/controllers/RegistryListCtrl.js
+++ app/controllers/RegistryListCtrl.js
@@ -1,33 +1,19 @@
 "use strict";
 
-app.controller("RegistryListCtrl", function($scope, $routeParms, ItemFactory, $route, SearchService) {
+app.controller("RegistryListCtrl", function($scope, ItemFactory, SearchService) {
   $scope.$watch(function () { return SearchService.getSearchText(); }, function (newValue, oldValue) {
      if (newValue !== null) {
          $scope.searchText= SearchService.getSearchText();
      }
    }, true);
-
-
-
-  let getRegistry =()=> {
-    let user = $scope.$parent.getUser2();
-    console.log("Registry List Ctrl", user);
-    ItemFactory.getItemList(user)
-    .then((itemCollectionArr) => {
-      $scope.items = itemCollectionArr;
-      console.log($scope.items);
-    })
-    .then ((items) =>{
-      $scope.$watch('items', function handleItemIndexChange(newValue, oldValue) {
-        ItemFactory.updateItem($scope.items);
-
-      }, true);
-    });
-  };
-
-
+  let user = $scope.$parent.getUser2();
+  console.log("Registry List Ctrl", user);
+  ItemFactory.getItemList(user)
+  .then((itemCollectionArr) => {
+    $scope.items = itemCollectionArr;
+    console.log($scope.items);
+  });
   $scope.itemDelete = (itemId) => {
-    console.log(itemId);
     ItemFactory.deleteItem(itemId)
     .then ((response) => {
       ItemFactory.getItemList(user)
